a4b33599a2d9c21cb24db66f1ca7b5b89e39b084
Vladimir Dima <vladimirdima@Vladimirs-MBP.lan>
2020-12-27 12:45:11 +0100

    initial comming with the project

---
+++ roughagency/wp-content/themes/twentytwentyone-child/template-parts/content/content-page.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<div class="coming-soon-wrapper">
+	<div class="left-side">
+		<div class="logo">
+<svg width="130px" height="75px" viewBox="0 0 45 26" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>Group</title>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Desktop-HD" transform="translate(-29.000000, -19.000000)" fill="#000000">
+            <g id="Group" transform="translate(29.000000, 19.000000)">
+                <text id="RØUGH" font-family="Metropolis-Bold, Metropolis" font-size="12" font-weight="bold">
+                    <tspan x="0" y="12">R</tspan>
+                    <tspan x="8.136" y="12" font-family="Metropolis-Medium, Metropolis" font-weight="400">Ø</tspan>
+                    <tspan x="17.844" y="12">UGH</tspan>
+                </text>
+                <text id="agency" font-family="EBGaramond-Regular, EB Garamond" font-size="16" font-weight="normal" letter-spacing="0.5">
+                    <tspan x="0" y="21">agency</tspan>
+                </text>
+            </g>
+        </g>
+    </g>
+</svg>
+		</div>
+		<div class="text">Our website is in progress but our artists are ready.</br> For any questions write us at <a href="mailto:info@roughagency.com">info@roughagency.com</a></div>
+	</div>
+
+	<div class="right-side">
+		<div class="right-side-inner">
+			<div class="coming-soon-text">is coming soon.</div>
+			<div class="social-icons">
+				<a href="#">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/instagram.svg" alt="instagram logo">
+				</a>
+				<a href="#">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/facebook.svg" alt="facebook logo">
+				</a>
+				<a href="#">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/linkedin.svg" alt="linkedin logo">
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
